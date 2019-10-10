0028720a9aa19db67456e1a84ecf904e3ee9e747
satya7289 <satyaprakashaman60@gmail.com>
2019-10-10 18:31:30 +0530

    index page

--- InventoryManagement/urls.py
+++ InventoryManagement/urls.py
@@ -19,5 +19,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',TemplateView.as_view(template_name='error/404.html')),
+    path('',TemplateView.as_view(template_name='layout.html')),
 ]
