f52ea29235ddd5141b8ab6283bb2228bc3e9619c
satya7289 <satyaprakashaman60@gmail.com>
2020-06-04 13:41:56 +0530

    heroku

--- InventoryManagement/wsgi.py
+++ InventoryManagement/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InventoryManagement.settings')
-
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InventoryManagement.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InventoryManagement.deployment')
 application = get_wsgi_application()
